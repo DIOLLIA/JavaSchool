4c7a4a6707adbf43ee1ec1903b1b0274ff473336
DIOLLIA <diollia88@gmail.com>
2018-05-12 11:23:45 +0400

    add new Schedule() for testing

--- src/main/java/schedule/controller/ScheduleController.java
+++ src/main/java/schedule/controller/ScheduleController.java
@@ -5,12 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import schedule.controller.model.StationSearch;
 import schedule.dao.RouteDao;
-import schedule.entity.ScheduleEntity;
-import schedule.model.Route;
 import schedule.model.Schedule;
 import schedule.model.Station;
 import schedule.service.ScheduleService;
@@ -32,10 +29,11 @@ public class ScheduleController {
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     public ModelAndView searchStations(@ModelAttribute("stationSearch") StationSearch stationSearch) {
 
+        scheduleService.addSchedule(new Schedule());
         ModelAndView modelAndView = new ModelAndView("searchResult");
-       List<Schedule> schedules = scheduleService.findStations(stationSearch.getArrivalStation(), stationSearch.getDepartureStation());
-      modelAndView.addObject("searchResult", schedules);
-       Station station = stationService.findByName(stationSearch.getArrivalStation());
+        List<Schedule> schedules = scheduleService.findStations(stationSearch.getArrivalStation(), stationSearch.getDepartureStation());
+        modelAndView.addObject("searchResult", schedules);
+        Station station = stationService.findByName(stationSearch.getArrivalStation());
         return modelAndView;
 
     }
