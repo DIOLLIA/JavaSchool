fe47a3fc1b1bfffe86a8d556f9ab453c509a1b2f
DIOLLIA <diollia88@gmail.com>
2018-05-17 14:55:59 +0400

    create Passenger entity

---
+++ src/main/java/schedule/entity/PassengerEntity.java
@@ -0,0 +1,56 @@
+package schedule.entity;
+
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
+
+@Table
+@Entity
+public class PassengerEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "birth_date")
+    private DateTime birthDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public DateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(DateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+}
