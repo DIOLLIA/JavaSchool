2a15666c7edc1fdef1aba6dbfc5310f9c756445a
DIOLLIA <diollia88@gmail.com>
2018-06-17 22:00:25 +0400

    stable version 1.0

--- src/main/java/schedule/messenger/SimpleMessageSender.java
+++ src/main/java/schedule/messenger/SimpleMessageSender.java
@@ -6,6 +6,7 @@ import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.Session;
 
@@ -14,12 +15,26 @@ public class SimpleMessageSender implements MessageSender {
     @Autowired
     private JmsTemplate jmsTemplate;
 
+    /*    @Override
+        public void send(String message) {
+            jmsTemplate.send(new MessageCreator() {
+                public Message createMessage(Session session) throws JMSException {
+                    return session.createTextMessage(message);
+                }
+            });
+        }*/
     @Override
     public void send(String message) {
         jmsTemplate.send(new MessageCreator() {
             public Message createMessage(Session session) throws JMSException {
-                
-                return session.createTextMessage(message);
+                MapMessage mapMessage = session.createMapMessage();
+                mapMessage.setString("Station","devyatkino");
+                mapMessage.setInt("train",2);
+                mapMessage.setInt("dailyId",1);
+                mapMessage.setString("depTime","19:00");
+                mapMessage.setString("arrTime","18:50");
+                return mapMessage;
+
             }
         });
     }
