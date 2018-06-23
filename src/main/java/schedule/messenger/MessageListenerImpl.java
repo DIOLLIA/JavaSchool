638cdf696dbf743f9022568e514dbc5e63656c5d
DIOLLIA <diollia88@gmail.com>
2018-06-23 16:30:21 +0400

    add method that invoke on startup

--- src/main/java/schedule/messenger/MessageListenerImpl.java
+++ src/main/java/schedule/messenger/MessageListenerImpl.java
@@ -1,11 +1,21 @@
 package schedule.messenger;
 
+import schedule.service.api.ScheduleService;
+
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
 public class MessageListenerImpl implements MessageListener {
+
+    private ScheduleService scheduleService;
+
+    public MessageListenerImpl(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
     @Override
     public void onMessage(Message message) {
-        System.out.println("x");
+        System.out.println("Remote app stated! Sending schedule...");
+        scheduleService.sendAll(scheduleService.transform(scheduleService.getSchedule()));
     }
 }
